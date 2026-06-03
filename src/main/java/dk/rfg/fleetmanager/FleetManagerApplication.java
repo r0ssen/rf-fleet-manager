8a637408c717f22dd3503cc0377dbe167cece27a
Thomas Rossen <tros@nykredit.dk>
2026-06-03 17:50:47 +0200

    Testing

--- src/main/java/dk/rfg/fleetmanager/FleetManagerApplication.java
+++ src/main/java/dk/rfg/fleetmanager/FleetManagerApplication.java
@@ -2,8 +2,10 @@ package dk.rfg.fleetmanager;
 
 import jakarta.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class FleetManagerApplication {
@@ -14,6 +16,11 @@ public class FleetManagerApplication {
     @PostConstruct
     public void checkFlyway() {
         var resource = getClass().getClassLoader().getResource("db/migration/V1__init_schema.sql");
-        System.out.println(">>> Flyway migration file found: " + resource);
+        System.out.println(">>> Migration file in classpath: " + resource);
+        System.out.println(">>> Flyway enabled property: " + env.getProperty("spring.flyway.enabled"));
+        System.out.println(">>> Flyway locations property: " + env.getProperty("spring.flyway.locations"));
     }
+
+    @Autowired
+    private Environment env;
 }
