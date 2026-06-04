ec220296983a18d81f4b561c4fc7dd1bc6ff7bbf
Thomas Rossen <tros@nykredit.dk>
2026-06-04 08:29:48 +0200

    cleanup

--- src/main/java/dk/rfg/fleetmanager/FleetManagerApplication.java
+++ src/main/java/dk/rfg/fleetmanager/FleetManagerApplication.java
@@ -12,15 +12,4 @@ public class FleetManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FleetManagerApplication.class, args);
     }
-
-    @PostConstruct
-    public void checkFlyway() {
-        var resource = getClass().getClassLoader().getResource("db/migration/V1__init_schema.sql");
-        System.out.println(">>> Migration file in classpath: " + resource);
-        System.out.println(">>> Flyway enabled property: " + env.getProperty("spring.flyway.enabled"));
-        System.out.println(">>> Flyway locations property: " + env.getProperty("spring.flyway.locations"));
-    }
-
-    @Autowired
-    private Environment env;
 }
